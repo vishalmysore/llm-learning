19c7a776946e41bb6aedf6acf67931910f5bbd81
Vishal Row Mysore <visrow@gmail.com>
2023-06-23 14:29:02 -0400

    historical prices

--- marketdata.py
+++ marketdata.py
@@ -4,7 +4,7 @@ from langchain.chains import RetrievalQA
 from langchain.llms import OpenAI
 import pandas as pd
 import os
-loader = CSVLoader(file_path='news/Historical_Stock_Prices.csv')  
+loader = CSVLoader(file_path='news/stocks.csv')  
 index_creator = VectorstoreIndexCreator()
 docsearch = index_creator.from_loaders([loader])
 chain = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.vectorstore.as_retriever(), input_key="question")
@@ -14,12 +14,15 @@ print(response['result'])
 query = "provide all unique ticker?"
 response = chain({"question": query})
 print(response['result'])
-query = "which company is this?"
+query = "provide name of company for each symbol"
 response = chain({"question": query})
 print(response['result'])
 query = "what was the highest stock price as per sheet?"
 response = chain({"question": query})
 print(response['result'])
-query = "which date was it? give in mm dd yyyy format"
+query = "do you see any voltality in any of the symbol?"
+response = chain({"question": query})
+print(response['result'])
+query = "can you create a test scenario based on cucumber?"
 response = chain({"question": query})
 print(response['result'])
